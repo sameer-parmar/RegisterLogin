cff32d8732bdc07195edad43d5495bf784d7ee12
sameer <sameerparmar.sp@gmail.com>
2024-07-11 15:51:19 +0530

    added basic frontend
    Signed-off-by: sameer <sameerparmar.sp@gmail.com>

---
+++ frontend-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>User Management System</h1>
+    <RegisterForm />
+    <LoginForm />
+    <!-- Example of using EmployeeDetails component -->
+    <EmployeeDetails username="exampleUser" /> <!-- Replace with actual username -->
+  </div>
+</template>
+
+<script>
+import RegisterForm from './components/RegisterForm.vue';
+import LoginForm from './components/LoginForm.vue';
+import EmployeeDetails from './components/EmployeeDetails.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RegisterForm,
+    LoginForm,
+   EmployeeDetails,
+  },
+};
+</script>
